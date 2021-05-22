f647c6134b21396e1c248a0f3d86b0b0ad40395e
Remi Salmon <salmon.remi@gmail.com>
2021-05-22 14:38:55 -0600

    Fix compatible-runtimes

--- deploy_layer.sh
+++ deploy_layer.sh
@@ -11,8 +11,8 @@ docker build -t "$LAMBDA_NAME" .
 docker run --rm -v $(pwd)/volume:/volume:z "$LAMBDA_NAME"
 
 aws lambda publish-layer-version --layer-name "$LAMBDA_NAME"-layer \
-                                 --description "python3.6 layer for $LAMBDA_NAME" \
-                                 --compatible-runtimes python3.6 \
+                                 --description "python3.8 layer for $LAMBDA_NAME" \
+                                 --compatible-runtimes python3.8 \
                                  --zip-file fileb://volume/layer.zip | tee log.json
 
 #aws lambda update-function-configuration --function-name "$LAMBDA_NAME" \
